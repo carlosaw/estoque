3e145dcf7eb0781ec62a0fa0d16714326903b89a
carlosaw <carlosfreevg@gmail.com>
2024-04-15 09:56:52 -0400

    environment

--- config.php
+++ config.php
@@ -11,12 +11,12 @@ if(ENVIRONMENT == 'development') {
 	$config['host'] = 'localhost';
 	$config['dbuser'] = 'root';
 	$config['dbpass'] = '';
-} else {
-	define("BASE_URL", "http://localhost/estoque/");
-	$config['dbname'] = 'nova_loja';
-	$config['host'] = 'localhost';
-	$config['dbuser'] = 'root';
-	$config['dbpass'] = 'root';
+} elseif(ENVIRONMENT == 'production') {
+	define("BASE_URL", "https://estoque.awregulagens.com.br/");
+	$config['dbname'] = 'estoque';
+	$config['host'] = '108.181.92.71';
+	$config['dbuser'] = 'estoque';
+	$config['dbpass'] = 'N1e2c3a4estoque';
 }
 
 $db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
